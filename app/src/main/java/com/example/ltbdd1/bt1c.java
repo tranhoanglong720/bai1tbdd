0359da4be6904c4f474d4eab3a3dd88de6db70b1
Tran Hoang Long <tranhoanglong720@gmail.com>
2022-02-14 17:15:54 +0700

    finish ui 1

---
+++ app/src/main/java/com/example/ltbdd1/bt1c.java
@@ -0,0 +1,14 @@
+package com.example.ltbdd1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bt1c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt1c);
+    }
+}
